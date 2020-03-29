859e2e51a63cd6e3f53522f0baddd547c997dac9
Gaurav Kumar Rai <34624495+gaurav09kumar@users.noreply.github.com>
2020-03-29 09:48:22 +0530

    Create Task1_Program3.py

---
+++ Task1_Program3.py
@@ -0,0 +1,13 @@
+#PythonCode
+
+#import math library
+import math
+
+#store pi value in a variable
+pi_value = math.pi;
+
+diameter = 12;
+radius = diameter/2;
+volume = (4/3)*pi_value*(radius**3);
+
+print("The volume of the sphere with diameter {0} is {1}".format(diameter, volume));
